c43c30b7d2ec6e090a359c0ec4beb9f46a942e1c
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-07-29 00:51:55 +0200

    fill environment file

--- workshop-july-2025/src/environments/environment.ts
+++ workshop-july-2025/src/environments/environment.ts
@@ -1,12 +1,14 @@
 export const environment = {
     production: false,
     firebase: {
-        apiKey: "<YOUR_GoogleAPIKey_HERE>",
-        authDomain: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
-        databaseURL: "https://workshop-angular-july-2025-default-rtdb.firebaseio.com/product",
-        projectId: "XXXXXXXXXXXXXXXXXXXXXX",
-        storageBucket: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
-        messagingSenderId: "XXXXXXXXXXXXX",
-        appId: '',
+        apiKey: "AIzaSyC7PTgr-VbN60zGobBj_SjjH4YDMwMfcb8",
+        authDomain: "workshop-angular-july-2025.firebaseapp.com",
+        databaseURL: "https://workshop-angular-july-2025-default-rtdb.firebaseio.com",
+        projectId: "workshop-angular-july-2025",
+        storageBucket: "workshop-angular-july-2025.firebasestorage.app",
+        messagingSenderId: "700604439001",
+        appId: "1:700604439001:web:1882055cd5086c68c261c8",
+        measurementId: "G-EJBKWZX1LK"
+
     },
 };
