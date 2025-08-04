4c324ed3fcaabf7e6ca2d2688e9070c64b2b70e3
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-08-04 23:03:09 +0200

    work with firebase

--- workshop-july-2025/src/app/app.component.ts
+++ workshop-july-2025/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ref, get} from '@angular/fire/database'; 
 
 @Component({
   selector: 'app-root',
@@ -11,6 +12,10 @@ export class AppComponent implements OnInit {
   title = 'Angular Project';
 
   ngOnInit(): void {
+
+    const x = get(ref('workshop-angular-july-2025' as any, 'post'));
+    console.log(x);
+    
     
   }
 }
--- workshop-july-2025/src/app/app.config.ts
+++ workshop-july-2025/src/app/app.config.ts
@@ -4,5 +4,8 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+
+  provideFirebaseApp(() => initializeApp()),
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), 
+  provideRouter(routes)]
 };
