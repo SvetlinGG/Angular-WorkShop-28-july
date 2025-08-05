274dcdbfb880a86e97831fd42464dc49f88f5800
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-08-05 23:13:38 +0200

    delete firebase files

--- workshop-july-2025/src/app/app.component.ts
+++ workshop-july-2025/src/app/app.component.ts
@@ -1,29 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { FirebaseService } from './firebase.service';
+
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
-  providers: [FirebaseService],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'Angular Project';
-
-  constructor(private firebaseService: FirebaseService){}
-
-  ngOnInit(): void {
-    this.serviceTest()
-
-    
-  }
-  serviceTest(){
-    this.firebaseService.getAll().subscribe(p => {
-      console.log(p);
-      
-    });
-    
-  }
+  
 }
--- workshop-july-2025/src/app/app.config.ts
+++ workshop-july-2025/src/app/app.config.ts
@@ -2,13 +2,14 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { initializeApp, provideFirebaseApp} from '@angular/fire/app'
+import { provideHttpClient } from '@angular/common/http';
+
 
 export const appConfig: ApplicationConfig = {
 
   
   providers: [
-    provideFirebaseApp(() => initializeApp()),
+    provideHttpClient(),
     provideZoneChangeDetection({ eventCoalescing: true }), 
     provideRouter(routes)
   ],
