2c9e92d4703a6b07951c60758da4aa44acccf31d
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-08-13 00:25:30 +0200

    routing components

--- workshop-july-2025/src/app/app.routes.ts
+++ workshop-july-2025/src/app/app.routes.ts
@@ -4,13 +4,20 @@ import { ErrorComponent } from './error/error.component';
 import { LoginComponent } from './user/login/login.component';
 import { RegisterComponent } from './user/register/register.component';
 import { ProfileComponent } from './user/profile/profile.component';
+//import { ThemesListComponent } from './theme/themes-list/themes-list.component';
+import { AddThemeComponent } from './theme/add-theme/add-theme.component';
+import { MainComponent } from './main/main.component';
 
 export const routes: Routes = [
     {path: '', redirectTo: '/home', pathMatch: 'full' },
     {path: 'home', component: HomeComponent },
+        // user routing
     {path: 'login', component: LoginComponent},
     {path: 'register', component: RegisterComponent},
     {path: 'profile', component: ProfileComponent},
+        // theme routing
+    {path: 'themes', component: MainComponent},
+    {path: 'add-theme', component: AddThemeComponent},
     // {path: '**', redirectTo: '/404'},
     {path: '**', component: ErrorComponent},
 ];
