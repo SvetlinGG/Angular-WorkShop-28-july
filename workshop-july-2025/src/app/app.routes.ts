dcdfbfec4a5c9c8570cec52ce068436c753fb96a
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-08-13 00:10:56 +0200

    routing components

--- workshop-july-2025/src/app/app.routes.ts
+++ workshop-july-2025/src/app/app.routes.ts
@@ -1,10 +1,16 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ErrorComponent } from './error/error.component';
+import { LoginComponent } from './user/login/login.component';
+import { RegisterComponent } from './user/register/register.component';
+import { ProfileComponent } from './user/profile/profile.component';
 
 export const routes: Routes = [
     {path: '', redirectTo: '/home', pathMatch: 'full' },
     {path: 'home', component: HomeComponent },
+    {path: 'login', component: LoginComponent},
+    {path: 'register', component: RegisterComponent},
+    {path: 'profile', component: ProfileComponent},
     // {path: '**', redirectTo: '/404'},
     {path: '**', component: ErrorComponent},
 ];
