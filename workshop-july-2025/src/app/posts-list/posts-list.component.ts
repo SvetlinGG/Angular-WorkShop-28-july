d666d8d2c0cdc295462978acb909574d93febf12
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-08-13 00:33:55 +0200

    routing components

--- workshop-july-2025/src/app/posts-list/posts-list.component.ts
+++ workshop-july-2025/src/app/posts-list/posts-list.component.ts
@@ -4,6 +4,7 @@ import { Post } from '../types/post';
 
 @Component({
   selector: 'app-posts-list',
+  standalone: true,
   imports: [],
   templateUrl: './posts-list.component.html',
   styleUrl: './posts-list.component.css'
