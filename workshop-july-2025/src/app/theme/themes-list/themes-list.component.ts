cbea9b3d0df8f39780b76168841a50b940d1d0eb
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-08-12 22:37:16 +0200

    theme list

--- workshop-july-2025/src/app/theme/themes-list/themes-list.component.ts
+++ workshop-july-2025/src/app/theme/themes-list/themes-list.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { ApiService } from './api.service';
+import { ApiService } from '../../api.service';
 
 @Component({
   selector: 'app-themes-list',
+  standalone: true,
   imports: [],
   templateUrl: './themes-list.component.html',
   styleUrl: './themes-list.component.css'
