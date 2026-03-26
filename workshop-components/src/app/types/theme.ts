5f4ce19251fd080a16b2d73bc53ad992e32d31dd
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2026-03-26 23:56:05 +0100

    make a posts colection

--- workshop-components/src/app/types/theme.ts
+++ workshop-components/src/app/types/theme.ts
@@ -1,15 +1,5 @@
-interface User {
-    "themes": ["5fa64a9f2183ce1728ff371a"],
-    "posts": ["5fa64a9f2183ce1728ff371b"],
-    "_id": "5fa64a072183ce1728ff3719",
-    "tel": "+359885885885",
-    "email": "david@gmail.com",
-    "username": "David",
-    "password": "$2b$05$P7mXZNv.Hy1BXyxp.clQL.o/P5xdtrwbK9tw4UhWH73hf/LNDu0j2",
-    "created_at": "2020-11-07T07:17:27.217Z",
-    "updatedAt": "2020-11-07T07:20:00.055Z",
-    "__v": 0
-  };
+import { User } from "./user";
+
 
 export interface Theme {
         "subscribers": string[];
@@ -20,4 +10,4 @@ export interface Theme {
         "created_at": string;
         "updatedAt": string;
         "__v": number;
-      }
+}
