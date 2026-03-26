77afaa5cb028e718048ef3d9120a8d8b2cc7320d
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2026-03-26 01:34:23 +0100

    get posts

--- workshop-components/src/app/api.service.ts
+++ workshop-components/src/app/api.service.ts
@@ -1,9 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from '../environments/environment.development';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getPosts(){
+    const {apiUrl} = environment;
+    return this.http.get(`${apiUrl}/posts.json`);
+  }
 }
