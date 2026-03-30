b56e0309a1dff1ec39d23723e874c3a277636601
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2026-03-30 13:49:59 +0200

    setup login page

--- workshop-components/src/app/app.routes.ts
+++ workshop-components/src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './user/login/login.component';
 
 export const routes: Routes = [
-    {path: '', component: HomeComponent}
+    //{ path: '', redirectTo: '/home', pathMatch: 'full' },
+    {path: 'home', component: HomeComponent},
+    {path: 'login', component: LoginComponent}
 ];
