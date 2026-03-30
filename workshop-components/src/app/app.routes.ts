7738a64a665b4ba825d137723ac3e527c3a2f26e
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2026-03-30 21:30:36 +0200

    error path

--- workshop-components/src/app/app.routes.ts
+++ workshop-components/src/app/app.routes.ts
@@ -3,12 +3,20 @@ import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './user/login/login.component';
 import { RegisterComponent } from './user/register/register.component';
 import { ThemesListComponent } from './theme/themes-list/themes-list.component';
+import { ErrorComponent } from './error/error.component';
 
 
 export const routes: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full' },
     {path: 'home', component: HomeComponent},
+
+
     {path: 'login', component: LoginComponent},
     {path: 'register', component: RegisterComponent},
-    {path: 'themes', component: ThemesListComponent}
+
+
+    {path: 'themes', component: ThemesListComponent},
+
+    //{path: '**', redirectTo:'/404'},
+    {path: '404', component: ErrorComponent},
 ];
