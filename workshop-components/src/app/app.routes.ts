b334e246188cc25351760d3820e87e34a859d0a7
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2026-03-30 21:52:01 +0200

    routing components

--- workshop-components/src/app/app.routes.ts
+++ workshop-components/src/app/app.routes.ts
@@ -4,19 +4,24 @@ import { LoginComponent } from './user/login/login.component';
 import { RegisterComponent } from './user/register/register.component';
 import { ThemesListComponent } from './theme/themes-list/themes-list.component';
 import { ErrorComponent } from './error/error.component';
+import { ProfileComponent } from './user/profile/profile.component';
+import { AddThemeComponent } from './theme/add-theme/add-theme.component';
+import { MainComponent } from './main/main.component';
 
 
 export const routes: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full' },
     {path: 'home', component: HomeComponent},
 
-
+// User routing:
     {path: 'login', component: LoginComponent},
     {path: 'register', component: RegisterComponent},
+    {path: 'profile', component: ProfileComponent},
 
-
-    {path: 'themes', component: ThemesListComponent},
-
-    //{path: '**', redirectTo:'/404'},
+// Theme routing:
+    {path: 'themes', component: MainComponent},
+    {path: 'add-theme', component: AddThemeComponent},
+    
     {path: '404', component: ErrorComponent},
+    {path: '**', redirectTo:'/404'},
 ];
---
+++ workshop-components/src/app/user/user.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor() { }
+
+  // todo: save if user authenticated in locale storage
+}
