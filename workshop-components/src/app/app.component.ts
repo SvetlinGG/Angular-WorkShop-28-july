f7fa43c6edb2f23a4057b4d827c020ad4e059da8
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2026-03-30 11:29:32 +0200

    check path to home page

--- workshop-components/src/app/app.component.ts
+++ workshop-components/src/app/app.component.ts
@@ -4,6 +4,7 @@ import { FooterComponent } from "./core/footer/footer.component";
 import { HeaderComponent } from './core/header/header.component';
 import { MainComponent } from './main/main.component';
 
+
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet,HeaderComponent, FooterComponent,MainComponent],
