c8d8bbb1ca752ebcd558b137a4b280b36e0c19dd
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2026-03-30 13:57:35 +0200

    setup route paths

--- workshop-components/src/app/app.routes.ts
+++ workshop-components/src/app/app.routes.ts
@@ -1,9 +1,13 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './user/login/login.component';
+import { RegisterComponent } from './user/register/register.component';
+import { ThemesListComponent } from './themes-list/themes-list.component';
 
 export const routes: Routes = [
-    //{ path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
     {path: 'home', component: HomeComponent},
-    {path: 'login', component: LoginComponent}
+    {path: 'login', component: LoginComponent},
+    {path: 'register', component: RegisterComponent},
+    {path: 'themes', component: ThemesListComponent}
 ];
