bc1636054c0eca1cc4874d5259177a6f60eed50e
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2026-03-31 21:04:08 +0200

    make auth guard

--- workshop-components/src/app/app.routes.ts
+++ workshop-components/src/app/app.routes.ts
@@ -8,6 +8,7 @@ import { ProfileComponent } from './user/profile/profile.component';
 import { AddThemeComponent } from './theme/add-theme/add-theme.component';
 import { MainComponent } from './main/main.component';
 import { CurrentThemeComponent } from './theme/current-theme/current-theme.component';
+import { AuthGuard } from './guards/auth.guard';
 
 
 export const routes: Routes = [
@@ -24,7 +25,7 @@ export const routes: Routes = [
         {path: '', component: MainComponent},
         {path: ':themeId', component: CurrentThemeComponent}
     ]},
-    {path: 'add-theme', component: AddThemeComponent},
+    {path: 'add-theme', component: AddThemeComponent, canActivate: [AuthGuard]},
     
     {path: '404', component: ErrorComponent},
     {path: '**', redirectTo:'/404'},
